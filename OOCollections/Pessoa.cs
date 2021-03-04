61c0344f29f4dd5b64c3458da129d3c7455575fa
Ellen <ellenengineer@gmail.com>
2021-03-03 22:16:11 -0300

    ex collections

--- OOCollections/Pessoa.cs
+++ OOCollections/Pessoa.cs
@@ -4,13 +4,12 @@ using System.Text;
 
 namespace OOCollections
 {
-
     public class Pessoa
     {
         public string Nome { get; set; }
         public DateTime DtNascFund { get; set; }
         public string Email { get; set; }
 
-        public Endereco Endereco { get; set; }
+        public List<Endereco> Enderecos { get;set; }
     }
 }
--- OOCollections/Program.cs
+++ OOCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOCollections
 {
@@ -9,15 +10,45 @@ namespace OOCollections
             ClientePF objPessoaFisica = new ClientePF();
 
             objPessoaFisica.Nome = "Ellen";
-            objPessoaFisica.Endereco = new Endereco();
-            objPessoaFisica.Endereco.ID = 1;
-            objPessoaFisica.Endereco.TipoEndereco = TipoEndereco.Residencial;
-            objPessoaFisica.Endereco.Logradouro = "Av Teste, 300";
-            objPessoaFisica.Endereco.Complemento = "apto 114 bl A";
-            objPessoaFisica.Endereco.Bairro = "Centro";
-            objPessoaFisica.Endereco.Cidade = "Townsville";
+            objPessoaFisica.Enderecos = new List<Endereco>();
 
-            Console.WriteLine(objPessoaFisica.Nome + " mora em " + objPessoaFisica.Endereco.Consultar());
+
+            Endereco end1 = new Endereco();
+            end1.ID = 1;
+            end1.TipoEndereco = TipoEndereco.Residencial;
+            end1.Logradouro = "Av Teste, 300";
+            end1.Complemento = "apto 114 bl A";
+            end1.Bairro = "Centro";
+            end1.Cidade = "Townsville";
+
+            objPessoaFisica.Enderecos.Add(end1);
+
+            Endereco end2 = new Endereco();
+            end2.ID = 2;
+            end2.TipoEndereco = TipoEndereco.Comercial;
+            end2.Logradouro = "Av Dom Pedro II, 300";
+            end2.Complemento = "apto 256";
+            end2.Bairro = "Centro";
+            end2.Cidade = "Citytown";
+
+            objPessoaFisica.Enderecos.Add(end2);
+
+
+            Endereco end3 = new Endereco();
+            end3.ID = 3;
+            end3.TipoEndereco = TipoEndereco.Outros;
+            end3.Logradouro = "Rua Imperador II, 90";
+            end3.Complemento = "casa 1";
+            end3.Bairro = "Tijuca";
+            end3.Cidade = "Ville2";
+
+            objPessoaFisica.Enderecos.Add(end3);
+
+            foreach (var end in objPessoaFisica.Enderecos)
+            {
+                Console.WriteLine(objPessoaFisica.Nome + " mora em " + end.Consultar());
+            }
+
 
             Console.ReadLine();
         }
